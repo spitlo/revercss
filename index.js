cb6c1ef68ed9ecb70d408d833223d2a2da0640db
Jakob Bergman <jakob@aino.se>
2014-12-06 19:19:53 +0100

    Added support for stdin

--- index.js
+++ index.js
@@ -41,81 +41,76 @@ cli.main(function ( args, options ) {
     return str.trim().replace( rx, '' )
   }
 
-  var outputFile = function (file) {
-    cli.withInput(file, function (line, sep, eof) {
-      if ( !eof ) {
-        /*
-        * Input phase
-        */
-        if ( line.indexOf( '{' ) > -1 ) {
-          // Start of selector block
-          inside = true
-          declaration = line.split( '{' )[0].trim()
-          declarations[declaration] = []
-          _selectors = []
-        } else if ( line.indexOf( '}' ) > -1 ) {
-          // End of selector block
-          inside = false
-          declaration = ''
-          if ( line.split( '}' ).length > 1 ) {
-            selector = line.split( '}' )[0].trim()
-            selector && _selectors.push( removeTrailingChar( selector ) )
-          }
-        } else if ( inside ) {
-          // Inside selector block
-          _selectors.push( removeTrailingChar( line ) )
+  var handleInput = function (line, sep, eof) {
+    if ( !eof ) {
+      /*
+      * Input phase
+      */
+      if ( line.indexOf( '{' ) > -1 ) {
+        // Start of selector block
+        inside = true
+        declaration = line.split( '{' )[0].trim()
+        declarations[declaration] = []
+        _selectors = []
+      } else if ( line.indexOf( '}' ) > -1 ) {
+        // End of selector block
+        inside = false
+        declaration = ''
+        if ( line.split( '}' ).length > 1 ) {
+          selector = line.split( '}' )[0].trim()
+          selector && _selectors.push( removeTrailingChar( selector ) )
         }
-
-        if ( _selectors.length > 0 && declaration ) {
-          _selectors = _selectors.join( ',' ).trim().split( ',' )
-          _selectors.forEach( function ( tag ) {
-            if ( selectors[ tag ] ) {
-              if ( selectors[ tag ].indexOf( declaration ) < 0 ) {
-                selectors[ tag ].push( declaration )
-              }
-            } else {
-              selectors[ tag ] = [ declaration ]
-            }
-          } )
-        }
-
-      } else if (cli.args.length) {
-        cli.output('Revercss only supports one file at a time.') // For now ...
-        //outputFile(cli.args.shift())
-      } else {
-        /*
-        * Output phase
-        */
-
-        _.forEach( selectors, function( declarations, selector ) {
-          outputCss += [ selector, '{', selectorSep ].join( space )
-          declarations.forEach( function( declaration ) {
-            outputCss += [ declarationPrefix, declaration, declarationSep ].join( '' )
-          } )
-          outputCss += '}' + lineSep
-        } )
-        
-        try {
-          if ( options.output ) {
-            outputStream = this.native.fs.createWriteStream( options.output )
-          } else {
-            outputStream = process.stdout
-          }
-          outputStream.write( outputCss )
-        } catch (e) {
-          this.fatal('Could not write to output stream.')
-        }
-
-        if ( options.echo ) {
-          console.log( outputCss )
-        }
-
+      } else if ( inside ) {
+        // Inside selector block
+        _selectors.push( removeTrailingChar( line ) )
       }
-    })
+
+      if ( _selectors.length > 0 && declaration ) {
+        _selectors = _selectors.join( ',' ).trim().split( ',' )
+        _selectors.forEach( function ( tag ) {
+          if ( selectors[ tag ] ) {
+            if ( selectors[ tag ].indexOf( declaration ) < 0 ) {
+              selectors[ tag ].push( declaration )
+            }
+          } else {
+            selectors[ tag ] = [ declaration ]
+          }
+        } )
+      }
+
+    } else if (cli.args.length) {
+      cli.output('Revercss only supports one file at a time.')
+    } else {
+      /*
+      * Output phase
+      */
+
+      _.forEach( selectors, function( declarations, selector ) {
+        outputCss += [ selector, '{', selectorSep ].join( space )
+        declarations.forEach( function( declaration ) {
+          outputCss += [ declarationPrefix, declaration, declarationSep ].join( '' )
+        } )
+        outputCss += '}' + lineSep
+      } )
+      
+      try {
+        if ( options.output ) {
+          outputStream = this.native.fs.createWriteStream( options.output )
+        } else {
+          outputStream = process.stdout
+        }
+        outputStream.write( outputCss )
+      } catch (e) {
+        this.fatal('Could not write to output stream.')
+      }
+
+    }
   }
 
   if (cli.args.length) {
-    outputFile(cli.args.shift())
+    cli.withInput( cli.args.shift(), handleInput )
+  } else {
+    cli.withInput( handleInput )
   }
 
 })
